d75038972536d945fb4db502c8a42cfb3c0cc379
CSC <sainz.carolina@gmail.com>
2026-09-22 12:59:25 +0200

    feat: create database migrations

---
+++ database/migrations/2026_09_21_214548_create_members_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 45);
+            $table->date('date_of_birth')->nullable();
+            $table->enum('category',['home','personal','family_member','pet'])->default('family_member');
+            $table->string('color',7)->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('members');
+    }
+};
